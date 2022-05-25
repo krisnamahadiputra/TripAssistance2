ca1c80ae224f73c037681ccbaeb4eff916b209da
Aurelia Nana <aurel002gaveythus@gmail.com>
2022-05-26 01:47:13 +0800

    coba lia

---
+++ app/src/main/java/com/kuliah/tripassistance/ReligiActivity.java
@@ -0,0 +1,70 @@
+package com.kuliah.tripassistance;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.kuliah.tripassistance.ornament.OrnamentLayoutMargin;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WisataActivity extends AppCompatActivity implements ReligiAdapter.onSelectData {
+
+    RecyclerView rvReligi;
+    OrnamentLayoutMargin gridMargin;
+    ReligiAdapter kulinerAdapter;
+    List<ModelReligi> modelKuliner = new ArrayList<>();
+    Toolbar tbReligi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_religi);
+
+        tbReligi = findViewById(R.id.toolbarReligi);
+        tbReligi.setTitle("Daftar Religi Bali");
+        setSupportActionBar(tbReligi);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        rvReligi = findViewById(R.id.rvReligi);
+        GridLayoutManager mLayoutManager = new GridLayoutManager(this,
+                2, RecyclerView.VERTICAL, false);
+        rvReligi.setLayoutManager(mLayoutManager);
+        gridMargin = new OrnamentLayoutMargin(2, Tools.dp2px(this, 4));
+        rvReligi.addItemDecoration(gridMargin);
+        rvReligi.setHasFixedSize(true);
+
+        getReligi();
+    }
+
+    private void getReligi() {
+
+    @Override
+    public void onSelected(ModelWisata modelWisata) {
+        Intent intent = new Intent(WisataActivity.this, DetailWisataActivity.class);
+        intent.putExtra("detailWisata", modelWisata);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
